23a5f67e80c9a38e4aedca0201f953e74d7e8d72
Ashlesha Murali Iyer <Sha@Shas-MacBook-Air.local>
2021-07-22 16:28:05 +0530

    fist part of the story

---
+++ file1.js
@@ -0,0 +1,2 @@
+Once upon a time, there was a princess named Snow White who lived in the forest with her friends, the seven dwarves. One day, an old lady came to Snow White and gave her an apple. Trusting the old hag, Snow ate the apple and fell into eternal slumber.
+Moral of the story: Do not trust strangers.
